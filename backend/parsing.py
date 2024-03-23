a057f6d99663341cc1fce6fd80992242fa2cb375
NikitaA <shovirins@gmail.com>
2024-03-24 01:14:37 +0300

    Enable getting files from Front-end

--- backend/parsing.py
+++ backend/parsing.py
@@ -9,14 +9,13 @@ app = FastAPI()
 def parse_csv_with_multiple_parents(csv_content: str) -> List[Dict]:
     objects = {}
     reader = csv.reader(io.StringIO(csv_content), delimiter=',', quotechar='"')
-    headers = next(reader)  # Пропускаем заголовки
+    headers = next(reader)
 
     for values in reader:
-        if not values:  # Пропускаем пустые строки
+        if not values:
             continue
         object_id = int(values[0])
         objects[object_id] = dict(zip(headers, values))
-        # Обработка поля "Сделать после"
         after_field = values[-1]
         if after_field:
             objects[object_id]['Сделать после'] = list(map(int, after_field.split(',')))
@@ -28,8 +27,7 @@ def parse_csv_with_multiple_parents(csv_content: str) -> List[Dict]:
 
 @app.post("/upload/")
 async def upload_csv(file: UploadFile = File(...)):
-    filename = file.filename
-    if filename.endswith('.csv'):
+    if file.content_type == 'text/csv':
         try:
             csv_content = (await file.read()).decode('utf-8')
             data = parse_csv_with_multiple_parents(csv_content)
@@ -37,4 +35,4 @@ async def upload_csv(file: UploadFile = File(...)):
         except Exception as e:
             return {"error": f"Error processing CSV file: {str(e)}"}
     else:
-        return {"error": "Only CSV files are allowed."}
+        return {"error": "Incorrect file format. Please upload a CSV file."}
