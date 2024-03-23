4518b2b1d228ffda5673f2af8289194661627418
brulikmas <lalkamasbeats@gmail.com>
2024-03-24 01:44:39 +0300

    front_5 > убрал слеш

--- frontend/src/store/mainStore.js
+++ frontend/src/store/mainStore.js
@@ -132,7 +132,7 @@ export const useMainStore = defineStore('mainStore', {
   actions: {
     async updateTasksList() {
       try {
-        const response = await axios.get(`${this.serverUrl}upload/`);
+        const response = await axios.get(`${this.serverUrl}upload`);
         console.log(response)
         //this.sortedTasksArray = response.data;
       } catch (error) {
@@ -144,7 +144,7 @@ export const useMainStore = defineStore('mainStore', {
         let formData = new FormData();
         formData.append('file', file);
         const response = await axios.post(
-          `${this.serverUrl}upload/`, 
+          `${this.serverUrl}upload`, 
           formData,
           {
             headers: {
