9d75e46846598f6dab8788387cbced0874b594df
NikitaA <shovirins@gmail.com>
2024-03-24 00:00:28 +0300

    New version of parsing (edited again) that has changed according to the new data format

--- backend/parsing.py
+++ backend/parsing.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI, UploadFile, File
 from typing import List, Dict
+import csv
+import io
 
 app = FastAPI()
 
 
-def parse_csv_with_multiple_parents(csv_content: List[str]) -> List[Dict]:
+def parse_csv_with_multiple_parents(csv_content: str) -> List[Dict]:
     objects = {}
-    for row in csv_content:
-        if row.startswith('Идентификатор'):
-            headers = row.strip().split(',')
+    reader = csv.reader(io.StringIO(csv_content), delimiter=',', quotechar='"')
+    headers = next(reader)  # Пропускаем заголовки
+
+    for values in reader:
+        if not values:  # Пропускаем пустые строки
             continue
-        elif not row.strip():
-            continue
+        object_id = int(values[0])
+        objects[object_id] = dict(zip(headers, values))
+        # Обработка поля "Сделать после"
+        after_field = values[-1]
+        if after_field:
+            objects[object_id]['Сделать после'] = list(map(int, after_field.split(',')))
         else:
-            values = row.strip().split(',')
-            object_id = int(values[0])
-            objects[object_id] = dict(zip(headers, values))
-            if values[-1]:
-                objects[object_id]['Сделать после'] = list(map(int, values[-1].strip('"').split(',')))
-            else:
-                objects[object_id]['Сделать после'] = []
+            objects[object_id]['Сделать после'] = []
 
     return list(objects.values())
 
@@ -29,7 +31,7 @@ async def upload_csv(file: UploadFile = File(...)):
     filename = file.filename
     if filename.endswith('.csv'):
         try:
-            csv_content = (await file.read()).decode('utf-8').split('\n')
+            csv_content = (await file.read()).decode('utf-8')
             data = parse_csv_with_multiple_parents(csv_content)
             return {"data": data}
         except Exception as e:
